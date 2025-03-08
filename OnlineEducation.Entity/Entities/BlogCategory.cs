778bdbdc2aec23da163bd95b772c5e4c7bd622e6
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-03-08 22:19:09 +0300

    Entities Created

---
+++ OnlineEducation.Entity/Entities/BlogCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEducation.Entity.Entities
+{
+    public class BlogCategory
+    {
+        public int BlogCategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ OnlineEducation.Entity/Entities/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEducation.Entity.Entities
+{
+   public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string ImageUrl { get; set; }
+        public int CourseCategoryId { get; set; }
+        public CourseCategory Category { get; set; }
+        public decimal Price { get; set; }
+        public bool IsShown { get; set; }
+    }
+}
