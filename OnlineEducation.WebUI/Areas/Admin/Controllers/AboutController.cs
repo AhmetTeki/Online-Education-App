132a7227ad8094d1aedb5eaf41c66a6a17a33a74
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-03-12 14:47:58 +0300

    Admin Layout Düzenlenmesi

---
+++ OnlineEducation.WebUI/Areas/Admin/Controllers/AboutController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEducation.WebUI.Areas.Admin.Controllers
+{
+    [Route("[area]/[controller]/[action]/{id?}")]
+    [Area("Admin")]
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
