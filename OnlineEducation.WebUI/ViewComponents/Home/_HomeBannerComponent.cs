a27ac002eb385c6de9c466d93ede4cb60a64d221
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-03-19 15:45:54 +0300

    UI Layout ve ViewComponent Yapısı

---
+++ OnlineEducation.WebUI/ViewComponents/Home/_HomeBannerComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEducation.WebUI.ViewComponents.Home
+{
+    public class _HomeBannerComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
