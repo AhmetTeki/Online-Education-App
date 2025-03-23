b1a0c349163045bd1f56a224e22e26748d4fc337
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-03-23 18:40:41 +0300

    Anasayfada sosyal medya ViewComponent

---
+++ OnlineEducation.WebUI/ViewComponents/UILayout/_UILayoutHeaderSocialMedia.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.WebUI.DTOs.ContactDtos;
+using OnlineEducation.WebUI.DTOs.SocialMediaDtos;
+using OnlineEducation.WebUI.Helpers;
+
+namespace OnlineEducation.WebUI.ViewComponents.UILayout
+{
+    public class _UILayoutHeaderSocialMedia: ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultSocialMediaDto>>("socialmedias");
+            return View(values);
+        }
+    }
+}
