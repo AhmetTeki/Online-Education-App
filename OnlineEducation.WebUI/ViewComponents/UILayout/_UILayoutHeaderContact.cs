638f52abdc168b9c93232e04d5589eb1be6c8f04
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-03-23 18:22:37 +0300

    Anasayfada iletişim ViewComponent

---
+++ OnlineEducation.WebUI/ViewComponents/UILayout/_UILayoutHeaderContact.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.WebUI.DTOs.ContactDtos;
+using OnlineEducation.WebUI.Helpers;
+
+namespace OnlineEducation.WebUI.ViewComponents.UILayout
+{
+    public class _UILayoutHeaderContact : ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultContactDto>>("contacts");
+            return View(values);
+        }
+    }
+}
