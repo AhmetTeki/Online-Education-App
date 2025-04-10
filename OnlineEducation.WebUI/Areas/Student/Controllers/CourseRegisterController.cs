f4cde01bb1ffd53b4c9ae890304f8f49eb1f98b6
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-04-11 00:05:42 +0300

    Student Area içinde Course Register Api Consume

--- OnlineEducation.WebUI/Areas/Student/Controllers/CourseRegisterController.cs
+++ OnlineEducation.WebUI/Areas/Student/Controllers/CourseRegisterController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.Entity.Entities;
+using OnlineEducation.WebUI.DTOs.CourseRegisterDtos;
+using OnlineEducation.WebUI.Helpers;
 
 namespace OnlineEducation.WebUI.Areas.Student.Controllers
 {
-    public class CourseRegisterController : Controller
+    [Authorize(Roles = "Student")]
+    [Area("Student")]
+    public class CourseRegisterController(UserManager<AppUser> _userManager) : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var values = await _client.GetFromJsonAsync<List<ResultCourseRegisterDto>>("courseRegisters/GetMyCourses/" + user.Id);
+            return View(values);
         }
+
     }
 }
---
+++ OnlineEducation.WebUI/DTOs/CourseRegisterDtos/UpdateCourseRegisterDto.cs
@@ -0,0 +1,15 @@
+﻿using OnlineEducation.Entity.Entities;
+using OnlineEducation.WebUI.DTOs.CourseDtos;
+
+namespace OnlineEducation.WebUI.DTOs.CourseRegisterDtos
+{
+    public class UpdateCourseRegisterDto
+    {
+        public int CourseRegisterId { get; set; }
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+
+        public int CourseId { get; set; }
+        public ResultCourseDto Course { get; set; }
+    }
+}
