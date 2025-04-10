ec68bb820ea599355f676a76be8640f8e537eff5
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-04-10 16:36:26 +0300

    Testimonial alanı

---
+++ OnlineEducation.WebUI/Areas/Admin/Controllers/TestimonialController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.WebUI.DTOs.AboutDtos;
+using OnlineEducation.WebUI.DTOs.TestimonialDtos;
+using OnlineEducation.WebUI.Helpers;
+
+namespace OnlineEducation.WebUI.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    [Area("Admin")]
+    public class TestimonialController : Controller
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IActionResult> Index()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultTestimonialDto>>("testimonials");
+            return View(values);
+        }
+        public async Task<IActionResult> DeleteTestimonial(int id)
+        {
+            await _client.DeleteAsync($"testimonials/{id}");
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult CreateTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateTestimonial(CreateTestimonialDto dto)
+        {
+            await _client.PostAsJsonAsync("testimonials", dto);
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> UpdateTestimonial(int id)
+        {
+            var values = await _client.GetFromJsonAsync<UpdateTestimonialDto>($"testimonials/{id}");
+            return View(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateTestimonial(UpdateTestimonialDto dto)
+        {
+            await _client.PutAsJsonAsync("testimonials", dto);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ OnlineEducation.WebUI/ViewComponents/Home/_HomeTestimonialComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.WebUI.DTOs.BannerDtos;
+using OnlineEducation.WebUI.DTOs.TestimonialDtos;
+using OnlineEducation.WebUI.Helpers;
+
+namespace OnlineEducation.WebUI.ViewComponents.Home
+{
+    public class _HomeTestimonialComponent : ViewComponent
+    {
+        private readonly HttpClient _httpClient = HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _httpClient.GetFromJsonAsync<List<ResultTestimonialDto>>("testimonials");
+            return View(values);
+        }
+    }
+}
