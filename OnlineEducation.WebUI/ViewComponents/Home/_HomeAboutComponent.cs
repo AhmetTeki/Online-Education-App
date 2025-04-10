b32f0eab3dbda9a557e7a8002eaa0970500efe89
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-04-10 17:31:58 +0300

    Design Process

---
+++ OnlineEducation.WebUI/ViewComponents/Home/_HomeAboutComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.WebUI.DTOs.AboutDtos;
+using OnlineEducation.WebUI.DTOs.BannerDtos;
+using OnlineEducation.WebUI.Helpers;
+
+namespace OnlineEducation.WebUI.ViewComponents.Home
+{
+    public class _HomeAboutComponent: ViewComponent
+    {
+        private readonly HttpClient _httpClient = HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _httpClient.GetFromJsonAsync<List<ResultAboutDto>>("abouts");
+            return View(values);
+        }
+    }
+}
---
+++ OnlineEducation.WebUI/ViewComponents/Home/_HomeBlogComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEducation.WebUI.DTOs.BannerDtos;
+using OnlineEducation.WebUI.DTOs.BlogDtos;
+using OnlineEducation.WebUI.Helpers;
+
+namespace OnlineEducation.WebUI.ViewComponents.Home
+{
+    public class _HomeBlogComponent: ViewComponent
+    {
+        private readonly HttpClient _httpClient = HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _httpClient.GetFromJsonAsync<List<ResultBlogDto>>("blogs");
+            return View(values);
+        }
+    }
+}
